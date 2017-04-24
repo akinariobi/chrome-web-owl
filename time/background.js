6ac0f10d08057801581c5fa2494e65cbc5287c2e
Iuliia <yuliyanasher@gmail.com>
2017-04-24 18:46:03 +0200

    Update background.js

--- time/background.js
+++ time/background.js
@@ -1,14 +1,4 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-45267314-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-function clearStats() {
+function clearStats() { //github.com/navjagpal
   if (config.clearStatsInterval < 3600) {
     config.nextTimeToClear = 0;
     return;
